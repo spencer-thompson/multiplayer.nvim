7f362b29b08a510ae578db798493c7c051ef7241
Spencer <sthompson.dev@outlook.com>
2024-11-28 16:10:13 -0700

    refactoring and major updates

--- lua/multiplayer.lua
+++ lua/multiplayer.lua
@@ -1,53 +1,57 @@
 local M = {}
+local Multiplayer = {}
 
-M.ns_id = vim.api.nvim_create_namespace("Multiplayer")
-M.curpos = { 1, 1 }
+Multiplayer.ns_id = vim.api.nvim_create_namespace("Multiplayer")
+Multiplayer.curpos = { 1, 1 }
 
-M.cursor_ns_id = vim.api.nvim_create_namespace("MultiplayerCursor")
+Multiplayer.cursor_ns_id = vim.api.nvim_create_namespace("MultiplayerCursor")
 
-M.mark_options = { "a", "s", "d", "f" }
-M.used_marks = {}
-M.clients = {}
+Multiplayer.mark_options = { "a", "s", "d", "f" }
+Multiplayer.used_marks = {}
+Multiplayer.players = {}
 
-M.channel = nil
+Multiplayer.channel = nil
 
-M.next_unused_mark = function()
-	for _, m in ipairs(M.mark_options) do
-		if not M.used_marks[m] then
-			M.used_marks[m] = true
+Multiplayer.next_unused_mark = function()
+	for _, m in ipairs(Multiplayer.mark_options) do
+		if not Multiplayer.used_marks[m] then
+			Multiplayer.used_marks[m] = true
 			return m
 		end
 	end
 	return nil
 end
 
-M.setup = function(opts)
-	-- M.socket_address = vim.fn.serverstart("websocket")
+Multiplayer.setup = function(opts)
+	_G.Multiplayer = Multiplayer
+	-- Multiplayer.socket_address = vim.fn.serverstart("websocket")
 
 	-- highlight groups | see :h guifg
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor1", { fg = "white", bg = "white" })
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor2", { fg = "NvimLightCyan" })
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor3", { fg = "NvimLightGreen" })
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor4", { fg = "NvimLightMagenta" })
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor5", { fg = "NvimLightRed" })
-	vim.api.nvim_set_hl(M.cursor_ns_id, "MultiplayerCursor6", { fg = "NvimLightYellow" })
+	vim.api.nvim_set_hl(0, "MultiplayerCursor", { link = "Cursor" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor1", { fg = "white", bg = "white" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor2", { fg = "NvimLightCyan" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor3", { fg = "NvimLightGreen" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor4", { fg = "NvimLightMagenta" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor5", { fg = "NvimLightRed" })
+	vim.api.nvim_set_hl(Multiplayer.cursor_ns_id, "MultiplayerCursor6", { fg = "NvimLightYellow" })
 
-	-- M.username = vim.api.nvim_cmd({ "git", "config", "user.name" }, { output = true })
-	M.username = vim.system({ "git", "config", "user.name" }, { text = true }):wait().stdout
-	M.username = vim.trim(M.username)
+	-- Multiplayer.username = vim.api.nvim_cmd({ "git", "config", "user.name" }, { output = true })
 
 	vim.api.nvim_create_user_command("Multiplayer", function(args)
 		-- if args.fargs[1] == "show" then
-		-- 	vim.print(M.events)
+		-- 	vim.print(Multiplayer.events)
 		-- end
 		if args.fargs[1] == "stop" then
 			vim.api.nvim_clear_autocmds({ group = "Multiplayer" })
 		end
 
 		if args.fargs[1] == "host" then
+			Multiplayer.username = vim.system({ "git", "config", "user.name" }, { text = true }):wait().stdout
+			Multiplayer.username = vim.trim(Multiplayer.username)
+
 			local channel = vim.fn.sockconnect("tcp", "127.0.0.1:6666", { rpc = true })
 			vim.print(channel)
-			M.channel = channel
+			Multiplayer.channel = channel
 
 			local all_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 			vim.rpcrequest(channel, "nvim_buf_set_lines", 0, 0, -1, false, all_lines)
@@ -55,17 +59,17 @@ M.setup = function(opts)
 			-- local filename = vim.api.nvim_buf_get_name(0)
 			-- vim.rpcrequest(channel, "nvim_buf_set_name", 0, filename)
 
-			-- M.clients = vim.rpcrequest(channel, "nvim_list_chans")
+			-- Multiplayer.players = vim.rpcrequest(channel, "nvim_list_chans")
 
-			-- for _, c in ipairs(M.clients) do
+			-- for _, c in ipairs(Multiplayer.players) do
 			-- 	if c.client and c.client.name then
 			-- 		if c.client.name == "Multiplayer" then
-			-- 			M.used_marks[c.client.attributes.mark] = true
+			-- 			Multiplayer.used_marks[c.client.attributes.mark] = true
 			-- 		end
 			-- 	end
 			-- end
 			--
-			-- local next_mark = M.next_unused_mark()
+			-- local next_mark = Multiplayer.next_unused_mark()
 
 			vim.rpcrequest(
 				channel,
@@ -74,7 +78,7 @@ M.setup = function(opts)
 				{},
 				"host",
 				{},
-				{ git_username = M.username, buf = vim.api.nvim_get_current_buf() }
+				{ git_username = Multiplayer.username, buf = vim.api.nvim_get_current_buf() }
 			)
 
 			local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
@@ -83,27 +87,41 @@ M.setup = function(opts)
 			vim.api.nvim_create_autocmd("CursorMoved", {
 				desc = "Track Cursor Movement",
 				pattern = "*", -- for now
-				group = M.autocmd_group,
+				group = Multiplayer.autocmd_group,
 				callback = function()
+					-- vim.api.nvim_buf_clear_namespace(0, Multiplayer.cursor_ns_id, 0, -1)
 					local curpos = vim.api.nvim_win_get_cursor(0)
-					local buf = vim.api.nvim_get_current_buf()
+					-- local buf = vim.api.nvim_get_current_buf()
 
-					vim.rpcnotify(
-						channel,
+					vim.rpcrequest(
+						Multiplayer.channel,
 						"nvim_buf_set_mark",
 						0,
-						string.sub(M.username, 1, 1):lower(),
+						string.sub(Multiplayer.username, 1, 1):lower(),
 						curpos[1],
 						curpos[2],
 						{}
 					)
-					vim.rpcnotify(
+					vim.rpcrequest(
 						channel,
 						"nvim_exec_lua",
-						[[return require("multiplayer").send_marks(...)]],
-						{ M.username, M.cursor_ns_id }
+						[[return Multiplayer.send_marks(...)]],
+						{ Multiplayer.username, Multiplayer.cursor_ns_id, "MultiplayerCursor" }
 					)
-					-- vim.api.nvim_buf_set_extmark(0, )
+					-- for name, player in pairs(Multiplayer.players) do
+					-- 	-- if not player.username == Multiplayer.username then
+					-- 	local pos = vim.api.nvim_buf_get_mark(player.buf, player.mark)
+					-- 	print(pos[1], pos[2])
+					--
+					-- 	vim.api.nvim_buf_set_extmark(
+					-- 		player.buf,
+					-- 		Multiplayer.cursor_ns_id,
+					-- 		pos[1],
+					-- 		pos[2],
+					-- 		{ hl_group = "MultiplayerCursor", end_col = pos[2] + 1 }
+					-- 	)
+					-- 	-- end
+					-- end
 				end,
 			})
 
@@ -136,7 +154,7 @@ M.setup = function(opts)
 			vim.api.nvim_create_autocmd("ChanInfo", {
 				desc = "Detect New Client",
 				pattern = "*", -- for now
-				group = M.autocmd_group,
+				group = Multiplayer.autocmd_group,
 				callback = function(ev)
 					print(string.format("event fired: %s", vim.inspect(ev)))
 				end,
@@ -164,39 +182,46 @@ M.setup = function(opts)
 	})
 end
 
-M.host = function()
+Multiplayer.host = function()
+	Multiplayer.username = vim.system({ "git", "config", "user.name" }, { text = true }):wait().stdout
+	Multiplayer.username = vim.trim(Multiplayer.username)
+end
+
+Multiplayer.join = function(opts)
 	print("hello")
 end
 
-M.join = function(opts)
-	print("hello")
+Multiplayer.test = function()
+	-- vim.notify(Multiplayer.players)
+	vim.notify(string.format("clients: %s", vim.inspect(Multiplayer.players)))
 end
 
-M.test = function()
-	-- vim.notify(M.clients)
-	vim.notify(string.format("clients: %s", vim.inspect(M.clients)))
-end
-
-M.send_marks = function(username, ns_id)
-	for _, c in ipairs(M.clients) do
+Multiplayer.send_marks = function(username, ns_id, hl)
+	-- print("bigger test")
+	-- vim.print(vim.api.nvim_buf_get_mark(0, "s"))
+	for name, player in pairs(Multiplayer.players) do
 		-- if not c.client.attributes.git_username == username then
-		local markpos = vim.api.nvim_buf_get_mark(0, c.mark)
-		vim.rpcnotify(c.id, "nvim_buf_set_mark", c.client.attributes.buf, c.mark, markpos[1] + 2, markpos[2], {})
+		-- vim.rpcnotify(player.id, "nvim_buf_clear_namespace", player.buf, ns_id, 0, -1)
+		-- vim.api.nvim_buf_clear_namespace(0, Multiplayer.cursor_ns_id, 0, -1)
+		local markpos = vim.api.nvim_buf_get_mark(0, player.mark)
+		vim.print(markpos[1])
+		vim.rpcrequest(player.id, "nvim_buf_set_mark", player.buf, player.mark, markpos[1], markpos[2], {})
 		vim.rpcrequest(
-			c.id,
+			player.id,
 			"nvim_but_set_extmark",
-			c.client.attributes.buf,
+			player.buf,
 			ns_id,
-			markpos[1] + 2,
+			markpos[1],
 			markpos[2],
-			{ hl_group = "MultiplayerCursor1", end_row = markpos[1] + 2, end_col = markpos[2] + 1 }
+			{ hl_group = hl, end_col = markpos[2] + 1 }
 		)
+
 		-- end
 	end
 end
 
-M.send_data = function(username)
-	-- for _, c in ipairs(M.clients) do
+Multiplayer.send_data = function(username)
+	-- for _, c in ipairs(Multiplayer.players) do
 	-- 	if not c.client.attributes.git_username == username then
 	-- 	end
 	-- end
@@ -206,7 +231,7 @@ M.send_data = function(username)
 		-- 	vim.rpcnotify(channel, "nvim_buf_set_lines", 0, flc, llc, false, content)
 		-- end,
 		on_changedtick = function(changed_tick, buf, cgt)
-			for _, c in ipairs(M.clients) do
+			for _, c in pairs(Multiplayer.players) do
 				if not c.client.attributes.git_username == username then
 					local content = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 					vim.rpcnotify(c.id, "nvim_buf_set_lines", 0, 0, -1, false, content)
@@ -214,7 +239,7 @@ M.send_data = function(username)
 			end
 		end,
 		on_bytes = function(bytes, buf, cgt, srow, scol, bofc, oerow, oecol, oeblc, nerow, necol, neblc)
-			for _, c in ipairs(M.clients) do
+			for _, c in pairs(Multiplayer.players) do
 				if not c.client.attributes.git_username == username then
 					local content = vim.api.nvim_buf_get_text(buf, srow, scol, srow + nerow, scol + necol, {})
 					vim.rpcnotify(c.id, "nvim_buf_set_text", 0, srow, scol, srow + oerow, scol + oecol, content)
@@ -222,7 +247,7 @@ M.send_data = function(username)
 			end
 		end,
 		on_reload = function(reload, buf)
-			for _, c in ipairs(M.clients) do
+			for _, c in pairs(Multiplayer.players) do
 				if not c.client.attributes.git_username == username then
 					local content = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 					vim.rpcnotify(c.id, "nvim_buf_set_lines", c.client.attributes.buf, 0, -1, false, content)
@@ -233,11 +258,17 @@ M.send_data = function(username)
 	-- for _, c in ipairs()
 end
 
-M.server = function()
+Multiplayer.sync = function(players)
+	for name, info in pairs(players) do
+		Multiplayer.players[name] = info
+	end
+end
+
+Multiplayer.server = function()
 	vim.api.nvim_create_autocmd("ChanInfo", {
 		desc = "Detect New Client",
 		pattern = "*", -- for now
-		group = M.autocmd_group,
+		group = Multiplayer.autocmd_group,
 		callback = function(ev)
 			print("start")
 			-- local all_clients = vim.rpcrequest(channel, "nvim_list_chans")
@@ -245,15 +276,41 @@ M.server = function()
 			for _, client in ipairs(all_clients) do
 				if client.client and client.client.name then
 					if client.client.name == "Multiplayer" then
+						-- if client.client.attributes.git_username = Multiplayer.players.client.attributes.git_username then
+						--
+						--
+						-- end
 						client.mark = string.sub(client.client.attributes.git_username, 1, 1):lower()
-						table.insert(M.clients, client)
+						Multiplayer.players[client.client.attributes.git_username] = {
+							id = client.id,
+							buf = client.client.attributes.buf,
+							mark = client.mark,
+							type = client.client.type,
+							username = client.client.attributes.git_username,
+						}
+						-- local player = {
+						-- 	id = client.id,
+						-- 	buf = client.client.attributes.buf,
+						-- 	mark = client.mark,
+						-- 	type = client.client.type,
+						-- 	username = client.client.attributes.git_username,
+						-- }
+						-- Multiplayer.players[player.name] = player
+						-- table.insert(Multiplayer.players, player)
+						-- table.insert(Multiplayer.players, client)
+						-- vim.rpcrequest(client.id, "nvim_exec_lua", [[return Multiplayer.]])
 					end
 				end
 			end
-			-- M.clients = vim.rpcrequest(channel, "nvim_list_chans")
+
+			for _, player in pairs(Multiplayer.players) do
+				vim.rpcrequest(player.id, "nvim_exec_lua", [[return Multiplayer.sync(...)]], { Multiplayer.players })
+			end
+
+			-- Multiplayer.players = vim.rpcrequest(channel, "nvim_list_chans")
 			-- vim.notify(string.format("event fired: %s", vim.inspect(ev)))
 		end,
 	})
 end
 
-return M
+return Multiplayer
